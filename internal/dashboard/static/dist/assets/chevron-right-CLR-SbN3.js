9203addf06996e69f6b5129d723ebc221cbd843b
Ersin Koc <ersinkoc@gmail.com>
2026-05-24 21:47:57 +0300

    build(web): split dashboard routes

---
+++ internal/dashboard/static/dist/assets/chevron-right-CLR-SbN3.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-hlr4XRDp.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
---
+++ web/go.mod
@@ -0,0 +1,3 @@
+module github.com/nothingdns/nothingdns/web
+
+go 1.25.0
